b395c508b3bf2197a56fcc440429cbd105215d90
kukicl03 <luka.kukic2003@gmail.com>
2026-04-22 22:16:50 +0200

    Implementacija Write funkcija

--- Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
+++ Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
@@ -25,14 +25,39 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] request = new byte[12];
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.Length)), 0, request, 4, 2);
+            request[6] = CommandParameters.UnitId;
+            request[7] = CommandParameters.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusWriteCommandParameters)CommandParameters).OutputAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)(((ModbusWriteCommandParameters)CommandParameters).Value))), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> dict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            if (response[7] != CommandParameters.FunctionCode + 0x80)
+            {
+                var address = BitConverter.ToUInt16(response, 8);
+                var value = BitConverter.ToUInt16(response, 10);
+
+                address = (ushort)IPAddress.NetworkToHostOrder((short)address);
+                value = (ushort)IPAddress.NetworkToHostOrder((short)value);
+
+                dict.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, address), value);
+            }
+            else
+            {
+                HandeException(response[8]);
+            }
+
+            return dict;
         }
     }
 }
